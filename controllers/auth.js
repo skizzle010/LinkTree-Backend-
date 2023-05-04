a6d87f439a990bd3ac88c665f5eb815692d151cf
skizzle010 <raiutkarsh3219@gmail.com>
2023-05-04 23:07:40 +0530

    login fe implemented

--- controllers/auth.js
+++ controllers/auth.js
@@ -15,9 +15,10 @@ exports.registerUser = async (req, res, next) => {
 
     const user = await User.create({handle,email,password,role:category,links:[defaultLink]})
 
-    const token = jwt.sign({email:email},process.env.PASS_SEC)
+    const token = jwt.sign({email:email},process.env.JWT_SEC)
     return res.json({message:'success',status:'success','token':token,id:user._id})
   }catch(err){
+    console.log(err);
     if(err.code === '11000'){
       return res.json({message: "Try a different handle or email",status: 'error'})
     }
@@ -27,34 +28,41 @@ exports.registerUser = async (req, res, next) => {
 
 
 
-exports.login = async (req, res, next) => {
-  try {
-    const user = await User.findOne({ email: req.body.email });
-    if (!user) return next(createError(404, "User not found"));
-    const hashedPassword = CryptoJS.AES.decrypt(
-      user.password,
-      process.env.PASS_SEC
-    );
-    const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
-    if (originalPassword !== req.body.password)
-      return res.status(401).json("Wrong Credentials!");
 
-    const accessToken = jwt.sign(
-      {
-        id: user._id,
-        isAdmin: user.isAdmin,
-      },
-      process.env.JWT_SEC,
-      { expiresIn: "5d" }
-    );
-    res
-      .cookie("access_token", accessToken, {
-        httpOnly: true,
-      })
-      .status(200)
-      .json(user);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-};
+  exports.login = (req, res)=>{
+    const { email, password } = req.body;
+    try {
+        const user = User.findOne({email: email, password: password});
+        console.log(user);
+        if(!user){
+            return res.json({status: 'not found', error: 'Invalid credentials'})
+        }
+        const token = jwt.sign({email: email}, process.env.JWT_SEC);
+        return res.json({message: 'user found', status: 'success', 'token': token, id: user._id});
+    } catch (err) {
+        return res.json({message: err.message, status: 'error'});
+    }
+}
 
+
+  //   const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+  //   if (originalPassword !== req.body.password)
+  //     return res.status(401).json("Wrong Credentials!");
+
+  //   const accessToken = jwt.sign(
+  //     {
+  //       id: user._id,
+  //       isAdmin: user.isAdmin,
+  //     },
+  //     process.env.JWT_SEC,
+  //     { expiresIn: "5d" }
+  //   );
+  //   res
+  //     .cookie("access_token", accessToken, {
+  //       httpOnly: true,
+  //     })
+  //     .status(200)
+  //     .json(user);
+  // } catch (err) {
+  //   res.status(500).json(err);
+  // }
